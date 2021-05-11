3b6a13665eb71613ee21080f495f34f10e3804dd
Mayank Gupta <mayankgbrc@gmail.com>
2021-05-11 14:58:19 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # covid_django
+## Steps to run
+I guess Python and pip is already installed on your PC.
+Here are the steps to follow:
+- Clone the repo and go to the repo using CMD/Terminal
+- pip install virtualenv
+- virtualenv venv
+- For Windows: .\venv\Scripts\activate | For MacOS/Linux: source venv/bin/activate
+- pip install -r requirements.txt
+- python manage.py migrate
+- python manage.py runserver
+- Now open 127.0.0.0:8000 on any browser, enter city name, and it worked
---
+++ covid_django/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+import requests
+def corona(request):
+    if request.method=="POST":
+        city_name=request.POST.get("city")
+        url = "https://corona-virus-world-and-india-data.p.rapidapi.com/api_india"
+        headers = {
+            'x-rapidapi-host': "corona-virus-world-and-india-data.p.rapidapi.com",
+            'x-rapidapi-key': "21cf14a192msh204f296e47aa397p15bd5djsn552202dd0a7d"
+            }
+
+        response = requests.request("GET", url, headers=headers).json()
+        count = 0
+        data = {}
+        for each in response['state_wise']:
+            if int(response['state_wise'][each]['active']) :
+                for city in response['state_wise'][each]['district']:
+                    if city.lower() == city_name.lower():
+                        data = response['state_wise'][each]['district'][city]
+                        count = 1
+        context={"data":data, "count": count, "city_name": city_name.title()}
+        return render(request,"covid_result.html",context)
+    else:
+        return render(request,"city_form.html")
